762d592feba211b41541f1a120e6bc88570aa058
csamiselo <chisambwe.samiselo@gmail.com>
2020-03-16 14:23:48 +0200

    Initial commit for Name generator App using Flutter.

---
+++ android/app/src/main/kotlin/com/zm/projectoverwatch/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.zm.projectoverwatch
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
